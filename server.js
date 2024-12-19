72ef18df2324ec06c5aec00049153e026e4c8ed2
sami khan <109520665+practice5610@users.noreply.github.com>
2024-12-19 05:34:04 +0500

    wip test

--- server.js
+++ server.js
@@ -1,112 +1,50 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const mongoose = require("mongoose");
-const moment = require("moment");
-const path = require("path");
-const methodOverride=require("method-override")
-app.use(methodOverride("_method"))
 
-// لأزم تحدد هنا الانجين اللي هيشتغل مع السيرفر
+const path = require("path");
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 app.set("view engine", "ejs");
-//هنا لازم تفهم ان ملف الس هو حاجه ستاتيك بالنسبه للسيرفر فلازم تعرفها
+
 app.use(express.static("public"));
 
 app.use(express.json());
-// const bodyParser = require("body-parser");
 app.use(express.urlencoded({ extended: true }));
 
-const DB_CONNECTION = require("./config/connection");
-const userRoutes = require("./routes/userRoutes");
-const userModel = require("./models/userModel");
-const errorHandler=require("./middleware/errorsHandle")
-
-
-
+const getRandomQuery = () => {
+  const queries = [
+    "commercial trash bins",
+    "accredited online psychology programs",
+    "best online bachelor degree in accounting",
+    "cheap medical insurance for senior citizens",
+    "business team management software tools",
+    "online employee scheduling software workforce management",
+    "health information technology bachelor degree online",
+    "best online degrees for adults",
+    "closest jiffy lube near me",
+    "quote mortgage",
+    "enterprise information management software",
+    "homeowners insurance league city tx",
+  ];
+  return queries[Math.floor(Math.random() * queries.length)];
+};
+const getRandomImage = () => {
+  const imageIndex = Math.floor(Math.random() * 13) + 1; // Random number between 1 and 13
+  return `https://performdigi.com/img${imageIndex}.png`;
+};
 // start requests
 app.get("/", async (req, res) => {
-  // طيب هنا عشان نعرف نعمل سيرفر ريندر لازم نستخدم فيو انجن
-  // بنستحدم ريندر مكان سيند
-  // ممكن تاخد البارامترز بتاعتنا اللي هنحتاج نستخدمها في الملف ككود جافاسكريبت
-  //   هنستخدم مكتبه مومنت عشان نتحكم في شكل التاريخ
-  const findUsers = await userModel.find();
-  if (!findUsers) {
-    res.status(400);
-    throw new Error("Cannot find any user");
-  }
-  res.render("index", { arr: findUsers, moment: moment });
+  const query = getRandomQuery(); // Generate dynamic query
+  const image = getRandomImage(); // Generate random image link
+  res.render("index", { query, image }); // Pass query and image to EJS
+});
+app.get("/result.php", (req, res) => {
+  const query = req.query.q; // Get the 'q' parameter from the URL
+  res.render("result", { query }); // Render the EJS template with the query
 });
 
-// التوضيح موجود في ملف الويب
-app.get("/user/add.html", async (req, res) => {
-  res.status(200).render("user/add");
-});
-app.use("/user/add.html", require("./routes/userRoutes"));
-
-
-// 
-app.get("/user/:id", async (req, res) => {
-  const findUser = await userModel.findById(req.params.id);
-  if (!findUser) {
-    res.status(400);
-    throw new Error("Cannot find this user");
-  }
-  console.log(findUser)
-  res.status(200).render("user/view",{obj:findUser,moment:moment});
-});
-
-
-
-
-// 
-app.get("/user/edit.html",()=>{
-  res.render("user/edit")
-})
-app.get("/edit/:id", async (req, res) => {
-  const findUser = await userModel.findById(req.params.id);
-  if (!findUser) {
-    res.status(400);
-    throw new Error("Cannot find this user");
-  }
-  console.log(findUser)
-  res.status(200).render("user/edit",{obj:findUser,moment:moment});
-});
-
-// app.use("/edit/:id",require("./routes/userRoutes"))
-app.put("/edit/:id", async (req,res)=>{
-  const findUser = await userModel.findById(req.params.id);
-  if (!findUser) {
-    res.status(400);
-    throw new Error("Cannot find this user");
-  }
-  console.log(req.body)
-  const updateUser = await userModel.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.status(200).redirect("/")
-})
-
-app.delete("/delete/:id",async (req,res)=>{
-  const findUser = await userModel.findById(req.params.id);
-  if (!findUser) {
-    res.status(400);
-    throw new Error("Cannot find this user");
-  }
-  const deleteUser = await userModel.deleteOne({ _id: req.params.id });
-  res.status(200).redirect("/")
-})
-
-
-
-app.post("/search", async(req,res)=>{
-  const regex = new RegExp(`^${req.body.SearchTXT}`, 'i');
-const findUser=await userModel.find({username:regex})
-res.render("index", { arr: findUser, moment: moment });
-})
-
-app.use(errorHandler)
-// DB connect
-DB_CONNECTION();
 // start server
 const port = process.env.port || 3000;
 app.listen(port, () => {
